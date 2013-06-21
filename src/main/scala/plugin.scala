46ac3efe61195f763002d774df579c8940a0708d
softprops <d.tangren@gmail.com>
2013-06-20 22:15:28 -0400

    more user friendly

--- src/main/scala/plugin.scala
+++ src/main/scala/plugin.scala
@@ -8,19 +8,32 @@ import dispatch._, dispatch.Defaults
 object Plugin extends sbt.Plugin {
   import Keys._
 
-  val bintrayRepo = SettingKey[String](
-    "bintrayRepo", "Bintray repository to publish to. Defaults to 'maven'")
-  val bintrayPackageLabels = SettingKey[Seq[String]](
-    "bintrayPackageLabels", "List of labels associated with bintray package that will be added on auto package creation")
-  val bintrayCredentialsPath = SettingKey[File](
-    "bintrayCredentialsPath", "File containing bintray api credentials")
-  val bintrayPackageVersions = TaskKey[Unit](
-    "bintrayPackageVersions", "List bintray versions for the current package")
+  val bintray = TaskKey[String](
+    "bintray", "bintray-sbt")
+
+  val repository = SettingKey[String](
+    "repository", "Bintray repository to publish to. Defaults to 'maven'")
+
+  val packageLabels = SettingKey[Seq[String]](
+    "packageLabels", "List of labels associated with your bintray package")
+
+  val credentialsFile = SettingKey[File](
+    "credentialsFile", "File containing bintray api credentials")
+
+  val packageVersions = TaskKey[Seq[String]](
+    "packageVersions", "List bintray versions for the current package")
+
+  val changeCredentials = TaskKey[Unit](
+    "changeCredentials", "Change your current bintray credentials")
 
   /** Ensure user-specific bintray package exists */
   private def ensurePackageTask: Def.Initialize[Task[Unit]] =
-    (bintrayCredentialsPath, bintrayRepo, name,
-     description, bintrayPackageLabels, streams).map {
+    (credentialsFile in bintray,
+     repository in bintray,
+     name,
+     description in bintray,
+     packageLabels in bintray,
+     streams).map {
       case (creds, repo, name, desc, labels, out) =>
         ensuredCredentials(creds).map {
           case BintrayCredentials(user, key) =>
@@ -33,13 +46,13 @@ object Plugin extends sbt.Plugin {
         }.getOrElse("failed to retrieve bintray credentials")
     }
 
-  /** if credentials exists and the build user hasn't defined a publishTo,
-   *  set a user-speciic publishTo endpoint */
-  private def publishToBintrayOrDefault: Def.Initialize[Option[Resolver]] =
-    (publishTo, bintrayCredentialsPath,
-     bintrayRepo, name, streams).apply {
-      case (provided @ Some(_), _, _, _, out) => provided
-      case (_, creds, repo, pkg, out) =>
+  /** set a user-speciic publishTo endpoint */
+  private def publishToBintray: Def.Initialize[Option[Resolver]] =
+    (credentialsFile in bintray,
+     repository in bintray,
+     name,
+     streams).apply {
+      case (creds, repo, pkg, out) =>
         ensuredCredentials(creds, prompt = false).map {
           case BintrayCredentials(user, pass) =>
             Opts.resolver.publishTo(user, repo, pkg,
@@ -49,7 +62,8 @@ object Plugin extends sbt.Plugin {
 
   /** if credentials exist, append a user-specific resolver */
   private def appendBintrayResolver: Def.Initialize[Seq[Resolver]] =
-    (bintrayCredentialsPath, bintrayRepo).apply {
+    (credentialsFile in bintray,
+     repository in bintray).apply {
       (creds, repo) =>
         BintrayCredentials.read(creds).fold({ err =>
           println("bintray credentials %s is malformed".format(err))
@@ -61,12 +75,14 @@ object Plugin extends sbt.Plugin {
     }
 
   /** Lists versions of bintray packages corresponding to the current project */
-  private def bintrayPackageVersionsTask: Def.Initialize[Task[Unit]] =
-    (streams, bintrayCredentialsPath, bintrayRepo, name).map {
+  private def packageVersionsTask: Def.Initialize[Task[Seq[String]]] =
+    (streams,
+     credentialsFile in bintray,
+     repository in bintray, name).map {
       (out, creds, repo, name) =>
-        import org.json4s._
         ensuredCredentials(creds, prompt = true).map {
           case BintrayCredentials(user, pass) =>
+            import org.json4s._
             val pkg = Client(user, pass).repo(user, repo).get(name)
             out.log.info("fetching package versions for package %s" format name)
             val versions = for {
@@ -74,29 +90,56 @@ object Plugin extends sbt.Plugin {
               ("versions", JArray(versions)) <- fs
               JString(versionString) <- versions
             } yield {
-              out.log.info("- %s" format version)
-              version
+              out.log.info("- %s" format versionString)
+              versionString
             }
             versions
         }.getOrElse(Nil)
     }
+
+  /** assign credentials or ask for new ones */
+  private def changeCredentialsTask: Def.Initialize[Task[Unit]] =
+    (streams, credentialsFile in bintray).map {
+      (out, creds) =>
+        BintrayCredentials.read(creds).fold(sys.error(_), _ match {
+          case None =>
+            saveCredentials(creds)(requestCredentials())
+          case Some(BintrayCredentials(user, pass)) =>
+            saveCredentials(creds)(requestCredentials(Some(user), Some(pass)))
+        })
+    }
+
+  private def ensureCredentialsTask =
+    (streams, credentialsFile in bintray) map {
+      (out, creds) => ensuredCredentials(creds).get
+    }
+
+  private def requestCredentials(
+    defaultName: Option[String] = None,
+    defaultKey: Option[String] = None) = {
+    val name = Prompt("Enter bintray username%s" format(
+      defaultName.map(" (%s)".format(_)).getOrElse(""))).orElse(defaultName)
+    if (!name.isDefined) sys.error("bintray username required")
+    val pass = Prompt.descretely("Enter bintray API key %s" format(
+      defaultKey.map(_ => "(use current)").getOrElse("(under https://bintray.com/profile/edit)"))).orElse(defaultKey)
+    if (!pass.isDefined) sys.error("bintray API key required")
+    (name.get, pass.get)
+  }
+
+  private def saveCredentials(to: File)(creds: (String, String)) = {
+    println("saving credentials to %s" format to)
+    val (name, pass) = creds
+    BintrayCredentials.write(name, pass, to)
+    println("reload project for publishTo to take effect")
+  }
 
   private def ensuredCredentials(creds: File, prompt: Boolean = true): Option[BintrayCredentials] =
     BintrayCredentials.read(creds).fold(sys.error(_), _ match {
       case None =>
         if (prompt) {
           println("bintray-sbt requires your bintray credentials.")
-          val name = Prompt("Enter bintray username")
-          if (!name.isDefined) sys.error("bintray username required")
-          val pass = Prompt.descretely("Enter bintray API key")
-          if (!pass.isDefined) sys.error("bintray API key required")
-
-          Seq(name, pass).flatten match {
-            case Seq(name, pass) =>
-              println("saving credentials to %s" format creds)
-              BintrayCredentials.write(name, pass, creds)
-              ensuredCredentials(creds, prompt)
-          }
+          saveCredentials(creds)(requestCredentials())
+          ensuredCredentials(creds, prompt)
         } else {
           println("Missing bintray credentials %s. Some bintray features depend on this." format creds)
           None
@@ -104,23 +147,27 @@ object Plugin extends sbt.Plugin {
       case creds => creds
     })
 
-  private def ensureCredentialsTask =
-    (streams, bintrayCredentialsPath) map {
-      (out, creds) => ensuredCredentials(creds).get
-    }
-
   def bintrayPublishSettings: Seq[Setting[_]] = Seq(
-    bintrayCredentialsPath := Path.userHome / ".bintray" / ".credentials",
-    bintrayRepo := "maven",
-    bintrayPackageLabels := Nil,
-    publishTo <<= publishToBintrayOrDefault,
-    resolvers <++= appendBintrayResolver,
-    publish <<= publish.dependsOn(ensurePackageTask.dependsOn(ensureCredentialsTask)),
-    credentials <+= bintrayCredentialsPath.map(Credentials(_))
+    credentialsFile in bintray := Path.userHome / ".bintray" / ".credentials",
+    repository in bintray := "maven",
+    packageLabels in bintray := Nil,
+    description in bintray <<= description,
+    publishTo in bintray <<= publishToBintray,
+    resolvers in bintray <<= appendBintrayResolver,
+    credentials in bintray <<= (credentialsFile in bintray).map(Credentials(_) :: Nil)
+  ) ++ Seq(
+    resolvers <++= resolvers in bintray,
+    credentials <++= credentials in bintray,
+    publishTo <<= publishTo in bintray,
+    publish <<= publish.dependsOn(ensurePackageTask.dependsOn(ensureCredentialsTask))
+  )
+
+  def bintrayCommonSettings: Seq[Setting[_]] = Seq(
+    changeCredentials in bintray <<= changeCredentialsTask
   )
 
   def bintrayQuerySettings: Seq[Setting[_]] = Seq(
-    bintrayPackageVersions <<= bintrayPackageVersionsTask
+    packageVersions in bintray <<= packageVersionsTask
   )
 
   def bintrayResolverSettings: Seq[Setting[_]] = Seq(
@@ -128,5 +175,5 @@ object Plugin extends sbt.Plugin {
   )
 
   def bintraySettings: Seq[Setting[_]] =
-    bintrayResolverSettings ++ bintrayPublishSettings ++ bintrayQuerySettings
+    bintrayCommonSettings ++ bintrayResolverSettings ++ bintrayPublishSettings ++ bintrayQuerySettings
 }
