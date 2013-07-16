154cf78e561e544108c3969d3b5976c0183c03a1
softprops <d.tangren@gmail.com>
2013-07-15 20:12:31 -0400

    this or that

---
+++ src/main/scala/http.scala
@@ -0,0 +1,13 @@
+package bintray
+
+import dispatch._
+import com.ning.http.client.Response
+
+case class EitherHttp[A,B](
+  notFound: Response => A,
+  exists: Response => B)
+  extends (Response => Either[A, B]) {
+  def apply(r: Response): Either[A,B] =
+    if (r.getStatusCode == 404) Left(notFound(r))
+    else Right(exists(r))
+}
