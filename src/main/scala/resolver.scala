1c2debad498abeee75385c8705674e5fe75bdc42
softprops <d.tangren@gmail.com>
2013-07-17 08:18:43 -0400

    wrong put

--- src/main/scala/resolver.scala
+++ src/main/scala/resolver.scala
@@ -2,6 +2,7 @@ package bintray
 
 import java.io.File
 import java.net.URL
+import org.apache.ivy.core.module.descriptor.Artifact
 import org.apache.ivy.plugins.resolver.IBiblioResolver
 import org.apache.ivy.plugins.repository.{ AbstractRepository, Repository, TransferEvent }
 import org.apache.ivy.plugins.repository.url.URLResource
@@ -10,21 +11,20 @@ import dispatch._
 
 case class BintrayRepository(
   underlying: Repository, bty: Client#Repo#Package)
- extends AbstractRepository {
+  extends AbstractRepository {
   def getResource(src: String) = underlying.getResource(src)
   def get(src: String, dest: File) = underlying.get(src, dest)
-  override def put(src: File, dest: String, overwrite: Boolean) {
-    fireTransferInitiated(new URLResource(new URL(dest)),
-                          TransferEvent.REQUEST_PUT)
-    val (code, body) = bty.mvnUpload(dest, src, publish = true)(
+  override def put(artifact: Artifact, src: File, dest: String, overwrite: Boolean) {
+    println("repo put artifact %s src %s dest %s..." format(artifact, src, dest))
+    val destPath = new URL(dest).getPath.split('/').drop(4).mkString("/")
+    val (code, body) = bty.mvnUpload(destPath, src, publish = true)(
       new FunctionHandler({ r => (r.getStatusCode, r.getResponseBody) }))()
     if (code != 201) {
       println(body)
-      val ex = new RuntimeException("error uploading to %s: %s" format(dest, body))
-      fireTransferError(ex)
-      throw ex
+      throw new RuntimeException("error uploading to %s: %s" format(dest, body))
     }
   }
+
   def list(parent: String) = underlying.list(parent)
 }
 
@@ -36,6 +36,8 @@ case class BintrayResolver(
   setM2compatible(true)
   setRoot(url)
 
-  override def setRepository(repository: Repository) =
+  override def setRepository(repository: Repository) = {
+    println("setting repository wrapper for %s" format repository)
     super.setRepository(BintrayRepository(repository, bty))
+  }
 }
