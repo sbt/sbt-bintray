47deaa2ec9e5df5385d4ec79469ab8ef82f6ab67
softprops <d.tangren@gmail.com>
2015-02-15 16:51:05 -0500

    prefer https

--- src/main/scala/Opts.scala
+++ src/main/scala/Opts.scala
@@ -5,7 +5,7 @@ import bintry.Client
 
 object Opts {
   object resolver {
-    val jcenter = MavenRepository("BintrayJCenter", "http://jcenter.bintray.com")
+    val jcenter = MavenRepository("BintrayJCenter", "https://jcenter.bintray.com")
 
     def publishTo(repo: Client#Repo, pkg: Client#Repo#Package, version: String, mvnStyle: Boolean = true, isSbtPlugin: Boolean = false) =
       if (mvnStyle) new RawRepository(
@@ -18,9 +18,10 @@ object Opts {
 
     def mavenRepo(name: String) = repo(name, "maven")
 
+    // newer versions of sbt have an alternative built in.
     def repo(name: String, repo: String) =
       MavenRepository(
         s"Bintray-Resolve-$name-$repo",
-        s"http://dl.bintray.com/content/$name/$repo")
+        s"https://dl.bintray.com/content/$name/$repo")
   }
 }
