84c3a11c8b240fd0e8bd977309c3e6b1a8368c40
softprops <d.tangren@gmail.com>
2015-02-15 17:15:06 -0500

    deprecate bintray repo factories now that they are in available in sbt 0.13.6+. fixes #36

--- src/main/scala/Opts.scala
+++ src/main/scala/Opts.scala
@@ -1,10 +1,11 @@
 package bintray
 
-import sbt.{ MavenRepository, RawRepository }
+import sbt.{ MavenRepository, RawRepository, Resolver }
 import bintry.Client
 
 object Opts {
   object resolver {
+    @deprecated("use sbt.Resolver.jcenterRepo instead (available in sbt 0.13.6+)", since = "0.2.0")
     val jcenter = MavenRepository("BintrayJCenter", "https://jcenter.bintray.com")
 
     def publishTo(repo: Client#Repo, pkg: Client#Repo#Package, version: String, mvnStyle: Boolean = true, isSbtPlugin: Boolean = false) =
@@ -16,9 +17,10 @@ object Opts {
                            pkg.version(version),
                            sbt.Resolver.ivyStylePatterns.artifactPatterns))
 
+    @deprecated("""use sbt.Resolver.bintrayRepo(owner,"maven") instead (available in sbt 0.13.6+)""", since = "0.2.0")
     def mavenRepo(name: String) = repo(name, "maven")
 
-    // newer versions of sbt have an alternative built in.
+    @deprecated("use sbt.Resolver.bintrayRepo(owner,repo) instead (available in sbt 0.13.6+)", since = "0.2.0")
     def repo(name: String, repo: String) =
       MavenRepository(
         s"Bintray-Resolve-$name-$repo",
